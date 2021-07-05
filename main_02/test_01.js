337f6254da6ce3f606e0ead8505ed3be0885c5e7
衫薄色 <1771380243@qq.com>
2021-07-05 18:11:17 +0800

    1

--- main_02/test_01.js
+++ main_02/test_01.js
@@ -1,34 +1,97 @@
 function showStacks() {
-        Java.perform(function () {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+    Java.perform(function () {
+        send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+    });
+}
+
+function ttsz() {
+    Java.perform(function () {
+        Java.openClassFile("data/local/tmp/gson-2.8.5.dex").load()
+        var Gson = Java.use("com.google.gson.Gson")
+        var gson = Gson.$new()
+        var SwitchConfig = Java.use('mtopsdk.mtop.global.SwitchConfig');
+        SwitchConfig.zJ.overload().implementation = function () {
+            return false;
+        }
+
+        // var InnerProtocolParamBuilderImpl = Java.use("mtopsdk.mtop.protocol.builder.impl.InnerProtocolParamBuilderImpl")
+        // InnerProtocolParamBuilderImpl.buildParams.implementation = function (mtopContext) {
+        //     showStacks()
+        //     var ret = this.buildParams(mtopContext)
+        //     console.log("params", gson.toJson(ret))
+        //     console.log("*******************")
+        //     return ret
+        // }
+
+        // var InnerSignImpl = Java.use("mtopsdk.security.InnerSignImpl")
+        // InnerSignImpl.getUnifiedSign.implementation = function (params, ext, appKey, authCode, useWua, requestId) {
+        //     showStacks()
+        //     var ret = this.getUnifiedSign(params, ext, appKey, authCode, useWua, requestId)
+        //     console.log("params", gson.toJson(ret))
+        //     console.log("*******************")
+        //     return ret
+        // }
+        var current_application = Java.use('android.app.ActivityThread').currentApplication();
+        var context = current_application.getApplicationContext();
+        // // console.log(context)
+        // var WVWebPushService = Java.use("android.taobao.windvane.extra.uc.WVWebPushService")
+        // console.log(WVWebPushService)
+        // var methods = WVWebPushService.class.getDeclaredMethods();
+        // for (var i in methods) {
+        //     console.log(methods[i].toString());
+        // }
+
+        var UTUtdid = Java.use("com.ta.utdid2.device.UTUtdid")
+        var Base64 = Java.use("com.ta.utdid2.android.utils.Base64")
+        var sess = Java.use("android.provider.Settings")
+        var utut = UTUtdid.instance(context)
+        var res = utut.readUtdid()
+        // var dit = sess.System.getString(context, "mqBRboGZkQPcAkyk")
+        // var res = utut.generateUtdid()
+        // console.log(gson.toJson(res))
+        // console.log(Base64.encodeToString(res, 2))
+        console.log(res)
+        console.log(sess)
+        var methods = sess.class.getDeclaredMethods();
+        for (var i in methods) {
+            console.log(methods[i].toString());
+        }
+        return res
+
+        // var wvws = WVWebPushService.getInstance(context)
+        // console.log(wvws)
+        // wvws.init()
+        // var res = wvws.getUtdidBySdk()
+        // console.log(res)
+        // return res
+    })
+}
+
+function hook_all() {
+
+    Java.perform(function () {
+        Java.enumerateLoadedClasses({
+            //枚举已经加载的类
+            onMatch: function (name, handle) {
+                //每枚举一个类就会调用一次这个函数
+                console.log('\n***class\n' + name);
+                var clazz = Java.use(name);
+                console.log('\n***name' + clazz);
+                var methods = clazz.class.getDeclaredMethods();
+                for (var i = 0; i < methods.length; i++) {
+                    console.log('\n***methods\n' + methods[i])
+                }
+            },
+            onComplete: function () {
+                //所有的类枚举完了会调用这个函数(只会调用一次)
+                console.log('完成所有类的枚举')
+            }
         });
-    }
-Java.perform(function () {
-    Java.openClassFile("data/local/tmp/gson-2.8.5.dex").load()
-    var Gson = Java.use("com.google.gson.Gson")
-    var gson = Gson.$new()
+    });
+}
 
-    // var InnerProtocolParamBuilderImpl = Java.use("mtopsdk.mtop.protocol.builder.impl.InnerProtocolParamBuilderImpl")
-    // InnerProtocolParamBuilderImpl.buildParams.implementation = function (mtopContext) {
-    //     showStacks()
-    //     var ret = this.buildParams(mtopContext)
-    //     console.log("params", gson.toJson(ret))
-    //     console.log("*******************")
-    //     return ret
-    // }
+function main(){
+    ttsz()
+}
 
-    var InnerSignImpl = Java.use("mtopsdk.security.InnerSignImpl")
-    InnerSignImpl.getUnifiedSign.implementation = function (params, ext, appKey, authCode, useWua, requestId) {
-        showStacks()
-        var ret = this.getUnifiedSign(params, ext, appKey, authCode, useWua, requestId)
-        console.log("params", gson.toJson(ret))
-        console.log("*******************")
-        return ret
-    }
-
-
-    var SwitchConfig = Java.use('mtopsdk.mtop.global.SwitchConfig');
-    SwitchConfig.zJ.overload().implementation = function () {
-        return false;
-    }
-})
+setImmediate(main);
